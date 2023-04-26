49feeff746dcabaf910886a0371ff0a03e4972ad
abubakrali10 <bakrali175@yahoo.com>
2023-04-26 16:48:42 -0700

    function that convert number to string

---
+++ num_to_str.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * num_to_str - convert number to string
+ * @num: input number
+ * Return: pointer to string
+ */
+
+char *num_to_str(int num)
+{
+	int i = 0, j = 0;
+	char *str, temp;
+
+	str = malloc(sizeof(char) * 4);
+
+	if (str == NULL)
+	{
+		perror("error");
+		return (NULL);
+	}
+	do {
+		str[i] = num % 10 + '0';
+		num /= 10;
+		i++;
+	} while (num > 0);
+
+	str[i] = '\0';
+
+	for (j = 0; j < i; j++)
+	{
+		temp = str[j];
+		str[j] = str[i - 1];
+		str[i - 1] = temp;
+		i--;
+	}
+
+	return (str);
+}
