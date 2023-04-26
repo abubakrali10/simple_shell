a1089a9dbe3b900ada7d506e85bcfbb643229c17
amgadfikry <dr.amgad_sh92@yahoo.com>
2023-04-25 17:01:01 -0700

    add free to function

--- shell_exit.c
+++ shell_exit.c
@@ -3,19 +3,24 @@
 /**
  * shell_exit - exit the shell
  * @arr: first arg of a command
+ * @line: line get by getline function
  * Return: nothing
  */
 
-void shell_exit(char **arr)
+void shell_exit(char **arr, char *line)
 {
 	if (string_cmp(arr[0], "exit") == 0)
 	{
 		if (arr[1] == NULL)
 		{
+			free_array(arr);
+			free(line);
 			exit(EXIT_SUCCESS);
 		}
 		else
 		{
+			free_array(arr);
+			free(line);
 			exit(atoi(arr[1]));
 		}
 	}
