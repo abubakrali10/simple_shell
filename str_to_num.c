a06a5b4b17b69c5dbc781a4fef6f7a268d4098a2
amgadfikry <dr.amgad_sh92@yahoo.com>
2023-04-26 18:23:22 -0700

    fix error

--- str_to_num.c
+++ str_to_num.c
@@ -10,18 +10,12 @@ int str_to_num(char *str)
 {
 	int sign = 1, res = 0, i = 0;
 
-	if (str[i] == '0')
-		return (0);
 	while (str[i])
 	{
 		if (str[i] == '-')
 			sign *= -1;
 		else
-		{
-			if (str[i] <= '0' || str[i] >= '9')
-				return (-1);
 			res = (res * 10) + (str[i] - '0');
-		}
 		i++;
 	}
 
