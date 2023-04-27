89b3110555bc96bc207cfe37f25bcc5ad9c674cd
abubakrali10 <bakrali175@yahoo.com>
2023-04-26 17:31:02 -0700

    handling exit function with arguments

--- shell_exit.c
+++ shell_exit.c
@@ -1,52 +1,86 @@
 #include "main.h"
 
 /**
- * str_to_num - convert str to num
- * @str: string input
- * Return: number
+ * exit_arg - mange exit with arguments
+ * @arr: array of command
+ * @line: line reading
+ * @error: number of line
+ * @argv: array of main argument
+ * Return: 1 or nothing
  */
 
-int str_to_num(char *str)
+int exit_arg(char **arr, char *line, int error, char *argv[])
 {
-	int sign = 1, res = 0, i = 0;
+	int state;
+	char *c;
 
-	while (str[i])
+	state = str_to_num(arr[1]);
+	if (state >= 256)
 	{
-		if (str[i] == '-')
-			sign *= -1;
+		free_array(arr);
+		free(line);
+		exit(state - 256);
+	}
+	else if (state < 0)
+	{
+		c = num_to_str(error);
+		write(STDOUT_FILENO, argv[0], string_len(argv[0]));
+		write(STDOUT_FILENO, ": ", 2);
+		write(STDOUT_FILENO, c, string_len(c));
+		write(STDOUT_FILENO, ": ", 2);
+		write(STDOUT_FILENO, arr[0], string_len(arr[0]));
+		write(STDOUT_FILENO, ": Illegal number: ", 18);
+		write(STDOUT_FILENO, arr[1], string_len(arr[1]));
+		write(STDOUT_FILENO, "\n", 1);
+		free(c);
+		if (!isatty(STDIN_FILENO))
+		{
+			free_array(arr);
+			free(line);
+			exit(2);
+		}
 		else
-			res = (res * 10) + (str[i] - '0');
-		i++;
+			free_array(arr);
+	}
+	else
+	{
+		free_array(arr);
+		free(line);
+		exit(state);
 	}
 
-	return (res * sign);
+	return (1);
 }
 
 /**
  * shell_exit - exit the shell
  * @arr: first arg of a command
  * @line: line get by getline function
+ * @null_case: stat of command
+ * @error: number of line
+ * @argv: array of arguments
  * Return: nothing
  */
 
-void shell_exit(char **arr, char *line)
+int shell_exit(char **arr, char *line, int null_case, int error, char *argv[])
 {
-	int state;
-
 	if (string_cmp(arr[0], "exit") == 0)
 	{
 		if (arr[1] == NULL)
 		{
 			free_array(arr);
 			free(line);
-			exit(EXIT_SUCCESS);
+			if (null_case)
+				exit(EXIT_SUCCESS);
+			else
+				exit(127);
 		}
 		else
 		{
-			state = str_to_num(arr[1]);
-			free_array(arr);
-			free(line);
-			exit(state);
+			if (exit_arg(arr, line, error, argv))
+				return (1);
 		}
 	}
+
+	return (0)
 }
